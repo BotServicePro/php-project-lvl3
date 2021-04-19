f84bc476a261fbf28d48c3d51946457f3ddbe725
akarakin <bulik889@gmail.com>
2021-04-19 13:09:43 +0300

    Added correct data validator

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,69 +23,59 @@ Route::get('/', function (Request $request) { // главная страница
     return view('main', $params);
 })->name('main');
 
+//
+//Route::get('/urls/{id}', function ($id) { // инфа о единичном урле c редактированием и так далее
+//    echo 'Sinle url page';
+//    exit;
+//    $params = ['url' => [], 'messages' => []];
+//    return view('main', $params);
+//})->name('singleUrl');
+//
+//Route::post('/urls/{id}/edit', function (Request $request) { // пост запрос на редактирование урла
+//    $params = ['url' => [], 'errors' => [], 'messages' => []];
+//    return view('main', $params);
+//})->name('editUrl');
+//
+//
+//Route::get('/urls', function (Request $request) { // список всех линков
+//    $params = ['url' => [], 'errors' => [], 'messages' => []];
+//    return view('main', $params);
+//});
 
-function getHostFromUrl($url): string
-{ // функция получения только домена из линка
-    $parsedUrl = parse_url($url);
-    if (isset($parsedUrl['host'])) {
-        return $parsedUrl['host'];
+
+Route::post('/', function (Request $request) { // пост запрос на добавление урлов
+    $url = $request->input('url')['name']; // так же получили урл из запроса
+    $parsedUrl = parse_url($url); // распарсили урл из запроса
+    $rules = [ // создаем правила для валидации
+        // bail = при первой же ошибке остановить проверку
+        // required = требование на необходимость наличия урла
+        // url = распашивается урл и на корректность
+        // првоерка на длину урла
+        // проверка на уникальность линка, проверка в базе urls в столбе name
+        'url.name' => 'bail|required|url|max:100|unique:urls,name'
+    ];
+
+    $validator = Validator::make($request->all(), $rules); // валидируем входные данные
+    $errorMessage = $validator->errors()->first('url.name'); // получаем сообщения об ошибке
+    $errorMessage = str_replace('.name',  '', $errorMessage); // заменяем '.name' на пусто, хз как убрать по другому
+
+    if ($validator->fails()) { // если есть ошибки, делаем редирект на главную страницу и во влэш пишем ошибку
+        return redirect('/')
+            ->withErrors(flash($errorMessage)->error());
     }
-    echo 'ERROR FROM FUNCTION getHostFromUrl';
-}
 
-function getSchemeFromUrl($url): string
-{ // функция получения только протокола из линка
-    $parsedUrl = parse_url($url);
-    if (isset($parsedUrl['scheme'])) {
-        return $parsedUrl['scheme'];
-    }
-    echo 'ERROR FROM FUNCTION getSchemeFromUrl';
-}
-
-
-Route::post('/', function (Request $request) { // пост запрос на добавление линков
-    $url = $request['url']['name']; // получили линк из запроса
-    $parsedUrl = parse_url($url); // распарсили линк из запроса
-    $schemeFromRequestedUrl = getSchemeFromUrl($url); // получили протокол из запроса линка
-    $hostFromRequestedUrl = getHostFromUrl($url); // получили хост линка из запроса
-
-    if (!isset($schemeFromRequestedUrl) || !isset($hostFromRequestedUrl)) { // проверка, если нет протокола или самого тела линка
-        $params = ['messages' => flash('Incorrect url!')->error(), 'url' => $url];
-        return redirect()->route('main', $params);
-    }
-
-    $allLinksFromDB = DB::table('urls')->get(); // получили все данные из таблицы urls
-
-    foreach ($allLinksFromDB as $linkFromDB) { // перебираем все строки бд с доступом к полю name
-        $onlyHostFromDB = getHostFromUrl($linkFromDB->name); // получили хост перебираемого линка из бд
-        if ($onlyHostFromDB === $hostFromRequestedUrl) { // если хост перебираемого линка из бд совпал с хостом из запроса
-            $params = ['messages' => flash('Url is already exists!')->warning()];
-            return view('main', $params);
-        }
-    }
+    $valideUrl = "{$parsedUrl['scheme']}://{$parsedUrl['host']}";
 
     DB::table('urls')->insertGetId( // записываем в бд новый линк
-        ['name' => $url, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+        ['name' => $valideUrl, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
     );
+
+    // получаем ИД только что добавленного линка
+    $addedUrlID = DB::table('urls')->where('name', $valideUrl)->first()->id;
 
     $params = ['messages' => flash('Url was added!')->success()];
     return view('main', $params);
-})->name('postUrl');
+    //return route('singleUrl', ['id' => $addedUrlID]);
+})->name('urls.store');
 
-
-Route::get('/urls/{id}', function (Request $request) { // инфа о единичном урле c редактированием и так далее
-    $params = ['url' => [], 'messages' => []];
-    return view('main', $params);
-})->name('singleUrl');
-
-Route::post('/urls/{id}/edit', function (Request $request) { // пост запрос на редактирование урла
-    $params = ['url' => [], 'errors' => [], 'messages' => []];
-    return view('main', $params);
-})->name('editUrl');
-
-
-Route::get('/urls', function (Request $request) { // список всех линков
-    $params = ['url' => [], 'errors' => [], 'messages' => []];
-    return view('main', $params);
-});
 
