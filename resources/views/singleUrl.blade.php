5565810cd02f823ad889d44e0c98a0f341a9eefe
akarakin <bulik889@gmail.com>
2021-04-26 20:35:48 +0300

    renamed sections and added new tables

--- resources/views/singleUrl.blade.php
+++ resources/views/singleUrl.blade.php
@@ -4,7 +4,14 @@
 @section('title', 'Single url')
 
 
-@section('Link Data')
+@isset($messages) {{-- если в переменной есть какое либо значение--}}
+@section('messages')
+    @include('flash::message')
+@endsection
+@endisset
+
+
+@section('link_data')
     <h2>Site: {{ $urlData->name }}</h2>
     <div class="table-responsive">
         <table class="table table-bordered table-hover text-nowrap" style="width:100%">
@@ -25,9 +32,32 @@
 @endsection
 
 
-@isset($messages) {{-- если в переменной есть какое либо значение--}}
-    @section('messages')
-        @include('flash::message')
-    @endsection
-@endisset
+@section('checks_data')
+            <h2 class="mt-5 mb-3">Checks</h2>
+            <form action="/url/{{ $urlData->id }}/checks" method="post">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                <input type="submit" class="btn btn-primary" value="CHECK">
+            </form>
 
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover text-nowrap" style="width:100%">
+                    <tr>
+                        <th>ID</th>
+                        <th>Status code</th>
+                        <th>H1</th>
+                        <th>Keywords</th>
+                        <th>Description</th>
+                        <th>Checking time</th>
+                    </tr>
+                    @foreach ($checksData as $check)
+                        <tr>
+                        <td>{{ $check->id }}</td>
+                        <td>{{ $check->status_code }}</td>
+                        <td>{{ $check->h1 }}</td>
+                        <td>{{ $check->keywords }}</td>
+                        <td>{{ $check->description }}</td>
+                        <td>{{ $check->updated_at }}</td>
+                        </tr>
+                    @endforeach
+                </table>
+@endsection
