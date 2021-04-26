8cc161a9d1263b96ef784019097df8e63daa9856
akarakin <bulik889@gmail.com>
2021-04-26 14:05:10 +0300

    Added new tests

--- tests/Feature/UrlsTest.php
+++ tests/Feature/UrlsTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature;
 
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
@@ -17,60 +16,90 @@ class UrlsTest extends TestCase
      *
      * @return void
      */
+//    protected function setUp(): void
+//    {
+//        parent::setUp();
+//
+//        DB::table('urls')->insertGetId( // записываем в бд новый линк
+//            ['name' => 'http://google.ru', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+//        );
+//        DB::table('urls')->insertGetId( // записываем в бд новый линк
+//            ['name' => 'http://auto.ru', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+//        );
+//        DB::table('urls')->insertGetId( // записываем в бд новый линк
+//            ['name' => 'http://test.ru', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+//        );
+//    }
 
-    public function addTestDataToBase()
+
+    public function testIndex()
     {
-        DB::table('urls')->insertGetId( // записываем в бд новый линк
-            ['name' => 'http://google.ru', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
-        );
-        DB::table('urls')->insertGetId( // записываем в бд новый линк
-            ['name' => 'http://auto.ru', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
-        );
-        DB::table('urls')->insertGetId( // записываем в бд новый линк
-            ['name' => 'http://test.ru', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
-        );
+        $response = $this->get(route('main'));
+        $response->assertOk();
     }
 
-    public function testRequestMainPage()
+    public function testAllUrls()
     {
-        $response = $this->get('/');
-        $response->assertStatus(200);
+        $response = $this->get(route('allUrls'));
+        $response->assertOk();
     }
 
-    public function testRequestUrlsPage()
+    public function testSingleUrl()
     {
-        $response = $this->get('/urls');
-        $response->assertStatus(200);
+        $urlData = ['name' => "http://test.com"];
+        $this->post(route('urls.store'), ['url' => $urlData]);
+        $response = $this->get(route('singleUrl', ['id' => 1]));
+        $response->assertOk();
     }
 
-    public function testRequestSingleUrlPage()
-    {
-        self::addTestDataToBase();
-        $response = $this->get('/url/3');
-        $response->assertStatus(200);
-    }
-
-    public function testUrl()
+    public function testSingleUrlNotFound()
     {
         DB::table('urls')->insertGetId( // записываем в бд новый линк
             ['name' => 'http://123.ru', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
         );
-        $response = $this->get('/url/1');
-        $response->assertStatus(200);
+        $response = $this->get(route('singleUrl', ['id' => 88]));
+        $response->assertStatus(500);
     }
 
-//    public function testPostUrl()
-//    {
-//        $response = $this->get(route('articles.create'));
-//        $response->assertOk();
-//    }
+    public function testStore(): void
+    {
+        $urlData = ['name' => 'https://example.com'];
+        $response = $this->post(route('urls.store'), ['url' => $urlData]);
+        $response->assertSessionHasNoErrors();
+        $this->assertDatabaseHas('urls', $urlData);
+    }
 
-//    public function testUrlBody()
-//    {
-//        DB::table('urls')->insertGetId( // записываем в бд новый линк
-//            ['name' => 'http://123.ru', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
-//        );
-//        $response = $this->get('/url/1');
-//        $response->assertSee("<td>http://123.ru</td>");
-//    }
+    public function testIncorrectStoreUrl(): void
+    {
+        $urlData = ['name' => 'https:gmail.com'];
+        $response = $this->post(route('urls.store'), ['url' => $urlData]);
+        $response->assertSessionHasErrors();
+    }
+
+    public function testTooLongStoreUrl(): void
+    {
+        $tooLongDomain = str_repeat('domain', 20);
+        $urlData = ['name' => "http://{$tooLongDomain}.com"];
+        $response = $this->post(route('urls.store'), ['url' => $urlData]);
+        $response->assertSessionHas('flash_notification.0.message', 'The url must not be greater than 100 characters.');
+    }
+
+    public function testInvalideStoreUrl(): void
+    {
+        $urlData = ['name' => "hp://test.com"];
+        $response = $this->post(route('urls.store'), ['url' => $urlData]);
+        $response->assertSessionHas('flash_notification.0.message', 'The url format is invalid.');
+    }
+
+    public function testAlreadyAddedStoreUrl(): void
+    {
+        DB::table('urls')->insertGetId( // записываем в бд новый линк
+            ['name' => 'http://test.com', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+        ); // заранее добавили запись в базу
+
+        $urlData = ['name' => "http://test.com"]; // сформировали новые данные
+        $response = $this->post(route('urls.store'), ['url' => $urlData, $urlData]); // запостили теже самые
+        $response->assertSessionHas('flash_notification.0.message', 'The url has already been taken.');
+    }
+
 }
