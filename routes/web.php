325b6d7355e8f5f8ab32ff16da219485294a1e9f
akarakin <bulik889@gmail.com>
2021-05-04 00:26:46 +0300

    Added exceptions, edited code, added mb_conver_encoding to exclude some errors

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\ConnectException;
 use GuzzleHttp\Exception\RequestException;
+use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
@@ -20,122 +21,115 @@ use DiDom\Document;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-// проверка на длину и обрезка
-//function lengthCheck($data, $offSet, $dataLength): string
-//{
-//    if (strlen($data) >= $dataLength) {
-//        $data = substr($data, $offSet, $dataLength);
-//        return "{$data}...";
-//    }
-//    return $data;
-//}
-
 Route::get('/', function () {
-    $params = ['url' => [], 'errors' => [], 'messages' => []];
+    $params = [
+        'url' => [],
+        'errors' => [],
+        'messages' => []
+    ];
     return view('main', $params);
 })->name('main');
 
 Route::get('/urls', function () {
-    // список всех линков
-    $urlsData = DB::table('urls')->orderBy('id', 'desc')->get();
+    $urlsData = DB::table('urls')
+        ->orderBy('id', 'asc')
+        ->get();
 
     $checksData = DB::table('url_checks')
         ->distinct('url_id')
         ->orderBy('url_id')
         ->orderBy('created_at', 'desc')
         ->get();
+
     $checksStatuses = $checksData->keyBy('url_id');
-
-    // ОТЛОВИТЬ ОШИБКУ НЕСУЩЕСТВУЮЩЕГО ИД линка
-
-    $params = ['urlsData' => $urlsData, 'errors' => [], 'messages' => [], 'checksStatuses' => $checksStatuses];
+    $params = [
+        'urlsData' => $urlsData,
+        'errors' => [],
+        'messages' => [],
+        'checksStatuses' => $checksStatuses
+    ];
     return view('allUrls', $params);
 })->name('allUrls');
 
 Route::post('/urls', function (Request $request) {
-    // пост запрос на добавление урлов
-    $url = strtolower($request->input('url')['name']); // так же получили урл из запроса и привели к нижнему регистру
-    $parsedUrl = parse_url($url); // распарсили урл из запроса
-    $rules = [ // создаем правила для валидации
-        // bail = при первой же ошибке остановить проверку
-        // required = требование на необходимость наличия урла
-        // url = распашивается урл и на корректность
-        // првоерка на длину урла
-        // проверка на уникальность линка, проверка в базе urls в столбе name
+    $url = strtolower($request->input('url')['name']);
+    $parsedUrl = parse_url($url);
+    $rules = [
         'url.name' => 'bail|required|url|max:100|unique:urls,name'
     ];
-    $request->session()->token(); // token
-    csrf_token(); // token
+    $request->session()->token();
+    csrf_token();
+    $validator = Validator::make($request->all(), $rules);
+    $errorMessage = $validator
+        ->errors()
+        ->first('url.name');
 
-
-    // ДОБАВИТЬ ПРОСТУЮ КАПЧКУ НА ВВОД ЛИНКА
-
-
-    $validator = Validator::make($request->all(), $rules); // валидируем входные данные
-    $errorMessage = $validator->errors()->first('url.name'); // получаем сообщения об ошибке
     // заменяем '.name' на пусто, хз как убрать по другому
     $errorMessage = str_replace('.name', '', $errorMessage);
-    if ($validator->fails()) { // если есть ошибки, делаем редирект на главную страницу и во влэш пишем ошибку
+    if ($validator->fails()) {
         if ($errorMessage === 'The url has already been taken.') {
             $id = DB::table('urls')
                 ->where('name', "{$parsedUrl['scheme']}://{$parsedUrl['host']}")
                 ->first()->id;
-            return redirect()->route('singleUrl', ['id' => $id])->withErrors(flash($errorMessage)->warning());
+            return redirect()
+                ->route('singleUrl', ['id' => $id])
+                ->withErrors(flash($errorMessage)
+                ->warning());
         }
-        return redirect('/')->withErrors(flash($errorMessage)->error());
+        return redirect('/')
+            ->withErrors(flash($errorMessage)
+                ->error());
     }
 
     $valideUrl = "{$parsedUrl['scheme']}://{$parsedUrl['host']}";
-
-    DB::table('urls')->insertGetId( // записываем в бд новый линк
-        ['name' => $valideUrl, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+    DB::table('urls')->insertGetId(
+        [
+            'name' => $valideUrl,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]
     );
 
-    // получаем ИД только что добавленного линка
     $addedUrlID = DB::table('urls')
-        ->where('name', $valideUrl)->first()->id;
-
-    //$params = ['messages' => flash('Url was added!')->success()];
-    flash('Url was added!')
-        ->success(); // добавляем сообщение
-    // редирект на именованную страницу с переданным параметром
+        ->where('name', $valideUrl)
+        ->first()->id;
+    flash('Url was added!')->success();
     return redirect()
         ->route('singleUrl', ['id' => $addedUrlID]);
 })->name('urls.store');
 
 Route::get('/url/{id}', function ($id) {
-    // получаем инфу о линке из бд
-    $urlData = DB::table('urls')->where('id', $id)->first();
+    $urlData = DB::table('urls')
+        ->where('id', $id)
+        ->first();
 
-    // делаем выборку проверок по одному линку c сортировкой по времени
+    if ($urlData === null) {
+        flash("Url does not exists!")->error();
+        return redirect()->route('allUrls')->setStatusCode(404);
+    }
+
     $checksData = DB::table('url_checks')
         ->where('url_id', '=', $id)
         ->orderBy('updated_at', 'desc')
         ->get();
-    $params = ['urlData' => $urlData, 'messages' => [], 'checksData' => $checksData];
+
+    $params = [
+        'urlData' => $urlData,
+        'messages' => [],
+        'checksData' => $checksData];
+
     return view('singleUrl', $params);
 })->name('singleUrl');
 
-
 Route::post('url/{id}/checks', function ($id) {
-
-
-    //$response = Http::get('http://mobbit.info')->body(); // получаем html страницу, код
-    //$response = Http::get('http://mobbit.info');
-    // получаем ссылку по которой идет проверка
-    $url = DB::table('urls')->where('id', '=', $id)->first()->name;
-//    Проверьте наличие тега <h1> на странице. Если он есть то запишите его содержимое в базу.
-//    Проверьте наличие тега <meta name="keywords" content="..."> на странице.
-//    Если он есть то запишите содержимое аттрибута content в базу.
-//    Проверьте наличие тега <meta name="description" content="..."> на странице.
-//    Если он есть то запишите содержимое аттрибута content в базу.
-//    Выведите эту информацию в списке проверок конкретного сайта.
+    $url = DB::table('urls')
+        ->where('id', '=', $id)
+        ->first()->name;
 
     try {
         $document = new Document($url, true);
     } catch (RuntimeException $e) {
-        flash("ConnectException: {$e->getMessage()}")->error(); // добавляем сообщение
+        flash("ConnectException: {$e->getMessage()}")->error();
         return redirect()->route('singleUrl', ['id' => $id]);
     }
 
@@ -171,6 +165,7 @@ Route::post('url/{id}/checks', function ($id) {
             $description = $document->find('*[property=og:description]')[0]->content;
         }
     }
+
     if (strlen($description) >= $dataLength) {
         $data = substr($description, $offSet, $dataLength);
         $description = "{$data}...";
@@ -181,33 +176,42 @@ Route::post('url/{id}/checks', function ($id) {
         $keywords = "{$data}...";
     }
     $client = new GuzzleHttp\Client();
+
     try {
         $client->request('GET', $url);
-        $statusCode = $client->request('GET', $url)->getStatusCode(); // получаем статус
+        $statusCode = $client
+            ->request('GET', $url)
+            ->getStatusCode();
     } catch (ConnectException $e) {
-        flash("ConnectException: {$e->getMessage()}")->error(); // добавляем сообщение
+        flash("ConnectException: {$e->getMessage()}")->error();
         return redirect()->route('singleUrl', ['id' => $id]);
     } catch (ClientException $e) {
         $statusCode = $e->getResponse()->getStatusCode();
     } catch (RequestException $e) {
-        flash("RequestException: {$e->getMessage()}")->error(); // добавляем сообщение
+        flash("RequestException: {$e->getMessage()}")->error();
         return redirect()->route('singleUrl', ['id' => $id]);
     }
 
-    DB::table('url_checks')->insert( // добавляем в таблицу запись
-        [
-            'url_id' => $id,
-            'status_code' => $statusCode,
-            'h1' => $h1,
-            'keywords' => $keywords,
-            'description' => $description,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]
-    );
-    DB::table('urls')
-        ->where('id', $id)
-        ->update(['updated_at' => Carbon::now()]);
-    flash('Url was checked')->message(); // добавляем сообщение
+    try {
+        DB::beginTransaction();
+        DB::table('url_checks')
+            ->insert([
+                'url_id' => $id,
+                'status_code' => $statusCode,
+                'h1' => mb_convert_encoding($h1, 'UTF-8'),
+                'keywords' => mb_convert_encoding($keywords, 'UTF-8'),
+                'description' => mb_convert_encoding($description, 'UTF-8'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        DB::table('urls')
+            ->where('id', $id)
+            ->update(['updated_at' => Carbon::now()]);
+        DB::commit();
+    } catch (QueryException $e) {
+        flash("RequestException: {$e->getMessage()}")->error();
+        return redirect()->route('singleUrl', ['id' => $id]);
+    }
+    flash('Url was checked')->message();
     return redirect()->route('singleUrl', ['id' => $id]);
 })->name('checkUrl');
