813b3d7079aec006501934e41ba79ff23f14a773
akarakin <bulik889@gmail.com>
2021-05-11 12:45:42 +0300

    Added correct pagination

--- resources/views/urls/index.blade.php
+++ resources/views/urls/index.blade.php
@@ -9,24 +9,27 @@
         <h1 class="mt-5 mb-3">Links</h1>
         <div class="table-responsive">
             <table class="table table-bordered table-hover text-nowrap">
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Last updated</th>
-            <th>Responsee answer</th>
-        </tr>
-        @foreach ($urlsData as $url)
-        <tr>
-            <td>{{ $url->id }}</td>
-            <td><a href="{{ route('show.url', ['id' => $url->id]) }}">{{ $url->name }}</a></td>
-            <td>{{ $url->updated_at }}</td>
-            <td>
-                @isset($checksData[$url->id]->status_code)
-                    {{  $checksData[$url->id]->status_code }}
-                @endisset
-            </td>
-        </tr>
-        @endforeach
-    </table>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Last updated</th>
+                    <th>Responsee answer</th>
+                </tr>
+                @foreach ($urlsData as $url)
+                    <tr>
+                        <td>{{ $url->id }}</td>
+                        <td><a href="{{ route('show.url', ['id' => $url->id]) }}">{{ $url->name }}</a></td>
+                        <td>{{ $url->updated_at }}</td>
+                    <td>
+                    @isset($checksData[$url->id]->status_code)
+                        {{  $checksData[$url->id]->status_code }}
+                    @endisset
+                    </td>
+                    </tr>
+                @endforeach
+            </table>
+            {{ $urlsData->links() }}
+        </div>
+    </div>
 @endsection
 
