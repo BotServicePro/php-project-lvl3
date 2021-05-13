d9bed80c18f0f7f1f5978afa674552735d0cf0c9
akarakin <bulik889@gmail.com>
2021-05-13 13:11:59 +0300

    Edited directory to load assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,9 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('public/js/app.js') }}" defer></script>
 </head>
 
 <body class="min-vh-100 d-flex flex-column">
