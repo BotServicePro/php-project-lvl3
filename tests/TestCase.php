14a3d5ec14f51c5297421e3d5a8c9d770f92d293
akarakin <bulik889@gmail.com>
2021-05-14 23:04:58 +0300

    Trate was moved out

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,13 +2,15 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+//use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 abstract class TestCase extends BaseTestCase
 {
-    use DatabaseTransactions;
+    // use DatabaseTransactions;
     use DatabaseMigrations;
     use CreatesApplication;
+    use RefreshDatabase;
 }
