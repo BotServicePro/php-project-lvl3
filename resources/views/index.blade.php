127522bfa83090471e5c63b9e40cb4fe34761e49
akarakin <bulik889@gmail.com>
2021-05-14 21:18:34 +0300

    Added named route

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,10 +12,9 @@
                     <div class="col-12 col-md-10 col-lg-8 mx-auto text-white">
                         <h1 class="display-3">Web page analyze</h1>
                         <p class="lead">Check web pages for SEO</p>
-                        <form action="/urls" method="post" class="d-flex justify-content-center">
+                        <form action="{{ route('urls.store') }}" method="post" class="d-flex justify-content-center">
                             @csrf
                             <label>
-                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                 <input type="text" size="70" name="url[name]" value="" class="form-control form-control-lg" placeholder="https://www.example.com">
                             </label>
                             <button type="submit" class="btn btn-lg btn-primary ml-3 px-5 text-uppercase">CHECK</button>
