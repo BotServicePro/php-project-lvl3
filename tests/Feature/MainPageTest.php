0b80bced434dfe6a49c1e28c9a1885d163c5fa04
akarakin <bulik889@gmail.com>
2021-05-14 23:11:24 +0300

    Added

---
+++ tests/Feature/MainPageTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class MainPageTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $response = $this->get(route('main.page'));
+        $response->assertOk();
+    }
+}
