ab71b66a6453000860f6119f7d9084c18e9674ca
akarakin <bulik889@gmail.com>
2021-05-18 10:42:39 +0300

    Edited test

--- tests/Feature/UrlsTest.php
+++ tests/Feature/UrlsTest.php
@@ -13,28 +13,30 @@ class UrlsTest extends TestCase
      *
      * @return void
      */
-//    public function testIndex()
-//    {
-//        $response = $this->get(route('urls.index'));
-//        $response->assertStatus(200);
-//    }
-//
-//    public function testShow()
-//    {
-//        $url = "http://test.com";
-//        $id = DB::table('urls')->insertGetId(
-//            ['name' => $url, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
-//        );
-//        $response = $this->get(route('show.url', ['id' => $id]));
-//        $response->assertStatus(200);
-//    }
+    public function testIndex()
+    {
+        $response = $this->get(route('urls.index'));
+        $response->assertStatus(200);
+    }
 
-//    public function testStore(): void
-//    {
-//        $urlData = ['name' => 'https://example.com'];
-//        $response = $this->post(route('urls.store'), ['url' => $urlData]);
-//        $response->assertSessionHasNoErrors();
-//        $response->assertRedirect(route('show.url', ['id' => 1]));
-//        $this->assertDatabaseHas('urls', $urlData);
-//    }
+    public function testShow()
+    {
+        $url = "http://test.com";
+        $id = DB::table('urls')->insertGetId(
+            ['name' => $url, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+        );
+        $response = $this->get(route('show.url', ['id' => $id]));
+        $response->assertStatus(200);
+        $response = $this->get(route('show.url', ['id' => 777]));
+        $response->assertStatus(404);
+    }
+
+    public function testStore(): void
+    {
+        $urlData = ['name' => 'https://example.com'];
+        $response = $this->post(route('urls.store'), ['url' => $urlData]);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect(route('show.url', ['id' => 1]));
+        $this->assertDatabaseHas('urls', $urlData);
+    }
 }
