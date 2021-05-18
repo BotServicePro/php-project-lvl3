8f68a5acafd5ef57c4aa269b7516351640952689
akarakin <bulik889@gmail.com>
2021-05-18 15:45:09 +0300

    Edited

--- tests/Feature/UrlsTest.php
+++ tests/Feature/UrlsTest.php
@@ -8,30 +8,34 @@ use Tests\TestCase;
 
 class UrlsTest extends TestCase
 {
-    private $id = 1;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-        DB::table('urls')->insert([
-            'id' => $this->id,
-            'name' => "http://test.com",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-    }
+//    private $id = 1;
+//
+//    protected function setUp(): void
+//    {
+//        parent::setUp();
+//        DB::table('urls')->insert([
+//            'id' => $this->id,
+//            'name' => "http://test.com",
+//            'created_at' => Carbon::now(),
+//            'updated_at' => Carbon::now()
+//        ]);
+//    }
 
     public function testIndex()
     {
         $response = $this->get(route('urls.index'));
-        $response->assertStatus(200);
+        $response->assertOk();
     }
 
     public function testShow()
     {
+        DB::table('urls')->insert([
+            'name' => "http://test.com",
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
         $response = $this->get(route('urls.show', ['id' => 1]));
-        $response->assertStatus(200);
-        dump($response->getStatusCode());
+        $response->assertOk();
     }
 
     public function testStore(): void
@@ -39,7 +43,7 @@ class UrlsTest extends TestCase
         $urlData = ['name' => 'https://example.com'];
         $response = $this->post(route('urls.store'), ['url' => $urlData]);
         $response->assertSessionHasNoErrors();
-        $response->assertRedirect(route('urls.show', ['id' => 2]));
+        $response->assertRedirect(route('urls.show', ['id' => 1]));
         $this->assertDatabaseHas('urls', $urlData);
     }
 }
